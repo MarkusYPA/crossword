07f5f240bf78c9c3f6fd01281ec98ec025603260
Markus laptop <markus.amberla@gritlab.ax>
2025-02-25 10:49:10 +0200

    half of first step

--- src/crosswordSolver.js
+++ src/crosswordSolver.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const { createArrayPuzzle, validInput, getStartPositions } = require('./solver/parser');
-const { solver } = require('./solver/solver');
+const { solver, updatePuzzle } = require('./solver/solver');
 
 const puzzle = '2001\n0..0\n1000\n0..0';
 const words = ['casa', 'alan', 'ciao', 'anta'];
@@ -33,7 +33,46 @@ function crosswordSolver(stringPuzzle, words) {
     }
 
     // Slightly faster when starting with long words
-    words.sort((a, b) => b.length - a.length);
+    //words.sort((a, b) => b.length - a.length);
+
+    const uniqueLengths = findUniques(words)
+    console.log(uniqueLengths)
+
+    if (uniqueLengths.length == 0) {
+        // fill slot with most crossings
+
+    } else {
+        // fill uniques
+        for (let i = 0; i < startPositions.length; i++) {
+            const start = startPositions[i]
+            for (let j = 0; j < uniqueLengths.length; j++) {
+                if (start.length == uniqueLengths[j].length) {
+                    arrayPuzzle = updatePuzzle(arrayPuzzle, uniqueLengths[j], start)  
+
+                    // one start was used
+                    startPositions = startPositions.slice(0, i).concat(startPositions.slice(i + 1))
+                    break
+                }
+            }
+        }
+
+        let newWords = []
+        for (const wd1 of words) {
+            let notUsed = true
+            for (const wd2 of uniqueLengths) {
+                if (wd1 == wd2) {
+                    notUsed = false
+                }
+            }
+            if (notUsed) newWords.push(wd1)
+        }
+
+        words = newWords
+
+        //printPuzzle(arrayPuzzle)
+    }
+
+    //return
 
     // Solver() finds solutions
     let solutions = [];
@@ -45,6 +84,20 @@ function crosswordSolver(stringPuzzle, words) {
     }
 
     printPuzzle(solutions[0]);
+}
+
+function findUniques(words) {
+    let uniqs = []
+    for (let i = 0; i < words.length; i++) {
+        let matches = 0
+        for (let j = 0; j < words.length; j++) {
+            if (words[i].length == words[j].length) {
+                matches++
+            }
+        }
+        if (matches < 2) uniqs.push(words[i])
+    }
+    return uniqs
 }
 
 module.exports = { crosswordSolver };
